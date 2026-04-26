02c3a8e977af26aac437db37fb98af9b992f8323
alakay13 <akhan101vc@gmail.com>
2026-04-26 17:31:49 -0400

    Added logging features: changed pom.xml file and Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,13 +1,25 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Calculator {
+    private static final Logger logger = LoggerFactory.getLogger(Calculator.class);
+
     public double calculate(double num1, double num2, String operation) {
-        switch (operation) {
-            case "Add":      return num1 + num2;
-            case "Subtract": return num1 - num2;
-            case "Multiply": return num1 * num2;
-            case "Divide":
-                if (num2 == 0) throw new ArithmeticException("Cannot divide by zero");
-                return num1 / num2;
-            default:         return 0;
+        logger.info("Calculated: {} {} {}", num1, operation, num2);
+
+        try {
+            switch (operation) {
+                case "Add":      return num1 + num2;
+                case "Subtract": return num1 - num2;
+                case "Multiply": return num1 * num2;
+                case "Divide":
+                    if (num2 == 0) throw new ArithmeticException("Cannot divide by zero");
+                    return num1 / num2;
+                default: return 0;
+        }
+        } catch (ArithmeticException e) {
+            logger.error("Mathematical error: {}", e.getMessage());
+            throw e;
         }
     }
 }
